d7bc5f1a8b1f06585c31abe0c2f85846bb273f89
talesofcaarter <kelvinjuma99@gmail.com>
2026-06-25 09:56:55 +0300

    refactor: Change the font to Google Sans

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -1,12 +1,13 @@
 import type { Metadata } from "next";
-import { Plus_Jakarta_Sans } from "next/font/google";
+import { Google_Sans } from "next/font/google";
 import "./globals.css";
 import { Toaster } from "@/components/ui/Toaster";
 import { Providers } from "./providers";
 
-const plusJakartaSans = Plus_Jakarta_Sans({
-  variable: "--font-plus-jakarta-sans",
+const googleSans = Google_Sans({
   subsets: ["latin"],
+  weight: ["400", "500", "700"],
+  variable: "--font-google-sans",
 });
 
 export const metadata: Metadata = {
@@ -20,10 +21,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html
-      lang="en"
-      className={`${plusJakartaSans.variable} h-full antialiased`}
-    >
+    <html lang="en" className={`${googleSans.variable} h-full antialiased`}>
       <body className="min-h-full flex flex-col">
         <Providers>{children}</Providers>
         <Toaster />
